fd0c7c30d960623a725ee539319b38b4d2a57533
davidtom <davidjtomczyk@gmail.com>
2017-05-26 17:33:07 -0400

    completed lab

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,52 @@
+require "pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = Hash.new
+  languages.each { |style, languages|
+    languages.each { |language, data|
+      new_hash[language] = data if !new_hash[language]
+      if !new_hash[language][:style]
+        new_hash[language][:style] = [style]
+      elsif !!new_hash[language][:style]
+        new_hash[language][:style] << style
+      else
+        puts "Something went wrong"
+      end
+    }
+  }
+  new_hash
 end
+
+puts reformat_languages(languages)
